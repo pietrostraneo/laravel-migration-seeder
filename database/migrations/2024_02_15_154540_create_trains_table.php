e9bf80948e89acbe2309c0e73d295f8b3e1b99da
pietrostraneo <straneo_p@yahoo.it>
2024-02-15 17:05:49 +0100

    Created tables on db by migration

--- database/migrations/2024_02_15_154540_create_trains_table.php
+++ database/migrations/2024_02_15_154540_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('arrival', 100);
+            $table->string('departure', 100);
+            $table->time('arrival_time');
+            $table->time('departure_time');
+            $table->integer('train_code');
+            $table->smallInteger('carriages');
+            $table->tinyInteger('on_time')->default('1');
+            $table->tinyInteger('ereased')->default('0');
             $table->timestamps();
         });
     }
